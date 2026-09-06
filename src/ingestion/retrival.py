1d56d9a5d2d396982f6145ab507c087affccaaa1
vardhan-U <chinimilli.venkatahemavardhan@gmail.com>
2026-09-06 20:16:15 +0530

    added and tested the generation and augmentation

--- src/ingestion/retrival.py
+++ src/ingestion/retrival.py
@@ -3,6 +3,7 @@ import numpy as np
 import streamlit as st
 from sentence_transformers import SentenceTransformer
 import json
+from openai import OpenAI
 emb1 = np.load(r"C:\Users\HI\Desktop\MY-PROJECTS\RAG-SERVICE-BOT\src\ingestion\embedding_store.npy")
 @st.cache_resource
 def load_model(path):
@@ -20,12 +21,12 @@ index = faiss.IndexFlatIP(d)
 index.add(xb)                  
 
 
-def search_query(query_embedding,k=4):
+def search_query(query_embedding,k=2):
 
     xq = np.array(query_embedding).astype('float32')
 
     D, I = index.search(xq, k)
-    
+    print("query search done")
     return D[0], I[0]
 
 def embed_query(query_question):
@@ -35,22 +36,48 @@ def embed_query(query_question):
     model = load_model(local_model_path)
 
     embedding = model.encode(query_question,show_progress_bar=True,normalize_embeddings=True)
+    print("embedding qury done")
     return embedding
 
 def query_related_chunk_ids(embedding):
     distances, indices = search_query(embedding.reshape(1, -1))
+    print("ids captured")
     return indices
 
-def print_related_chunks(indices):
+def related_chunks(indices):
     chunk_ids = np.load(r"C:\Users\HI\Desktop\MY-PROJECTS\RAG-SERVICE-BOT\src\ingestion\chunkids.npy")
+    relAted_chunks_list =[] 
     with open(r"C:\Users\HI\Desktop\MY-PROJECTS\RAG-SERVICE-BOT\data\processed\chunks.jsonl","r",encoding="utf-8") as f:
         all_chunks = [json.loads(line) for line in f]
         for idx in indices:
             target_id = chunk_ids[idx]
             match = next(c for c in all_chunks if c["chunk_id"] == target_id)
-            print(match["content"])
-            print("\n\n\n\n")
+            relAted_chunks_list.append(match["content"])
+            # print(match["content"])
+            # print("\n\n\n\n")
+    print("Chunks gathered")
+    return relAted_chunks_list
+# a=embed_query("""How can I allow only GET and POST requests from a specific frontend while allowing credentials?""")
+# b=query_related_chunk_ids(a)
+# print_related_chunks(b)
 
-a=embed_query("""How can I allow only GET and POST requests from a specific frontend while allowing credentials?""")
-b=query_related_chunk_ids(a)
-print_related_chunks(b)
+
+def generate_responese(chunks,query):
+    client = OpenAI(base_url="http://localhost:11434/v1",api_key="OLLAMA")
+    chunks_text = "\n\n".join(chunks)
+    messages = [{"role":"system","content":"Now you are an assistant who is going to help the user address his issue \
+                 .You are provided the user query followed by the related info.Send output only the response for the query ,donot explain"
+                 "the thought process .The output should only be the delivarable response.Keep the thinking latency minimal"},{
+                     "role":"user",
+                     "content":f"Now this is the query:{query} and these are chunks:{chunks_text}"
+                 }]
+    response = client.chat.completions.create(model="phi3:instruct",messages=messages)
+    print(response.choices[0].message.content)
+
+
+query = "How to split endpoints into multiple files using APIRouter in FastAPI"
+
+a = embed_query(query)
+b = query_related_chunk_ids(a)
+c = related_chunks(b)
+generate_responese(chunks=c,query=query)
